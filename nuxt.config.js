1475b4072dd926b291f72be35031681d28f98f88
Odedeyi Opeyemi <opeyemiodedeyi@gmail.com>
2023-05-15 15:47:44 +0100

    removed plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~plugins/getBlogDetails.js',
     '~plugins/shorten-text.js',
     '~plugins/swiper.js'
   ],
