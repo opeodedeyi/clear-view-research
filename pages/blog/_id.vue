8b5e474ac8ed2cd39871eaf5f9a245fc92e23746
Odedeyi Opeyemi <opeyemiodedeyi@gmail.com>
2023-06-05 13:32:53 +0100

    Update _id.vue

--- pages/blog/_id.vue
+++ pages/blog/_id.vue
@@ -44,6 +44,97 @@ import loadingb from "@/components/utilities/loadingb";
 import mainbutton from "@/components/utilities/mainbutton";
 
 export default {
+    async asyncData({ params, $contentful }) {
+        function getCustomDate(passedDate) {
+            const date = new Date(passedDate);
+            let year = date.getFullYear();
+            const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            let month = months[date.getMonth()];
+            let dt = date.getDate();
+
+            let newDate = `${dt} ${month}, ${year}`
+            return newDate
+        }
+
+        var response = await $contentful.client.getEntries({
+            content_type: 'blog',
+            'fields.slug': params.id
+        });
+        let blog = response.items;
+
+        blog = blog.map((item) => {
+            let renderOptions = {
+                renderNode: {
+                    [BLOCKS.EMBEDDED_ENTRY]: (node, children) => {
+                        if (node.data.target.sys.contentType.sys.id === "codeBlock") {
+                            return `<pre>
+                                <code>${node.data.target.fields.code}</code>
+                            </pre>`;
+                        }
+
+                        if (node.data.target.sys.contentType.sys.id === "videoEmbed") {
+                            return `<iframe
+                                src='${node.data.target.fields.embedUrl}'
+                                height='100%'
+                                width='100%'
+                                frameBorder='0'
+                                scrolling='no'
+                                title='${node.data.target.fields.title}'
+                                allowFullScreen=true
+                            />`;
+                        }
+                    },
+                    [INLINES.HYPERLINK]: (node, next) => {
+                        if (node.data.uri.includes("player.vimeo.com/video")) {
+                            return `<div class="iframe-container">
+                                <iframe
+                                title="${next(node.content)}"
+                                src="${node.data.uri}"
+                                frameBorder='0'
+                                allowFullScreen
+                                ></iframe>
+                            </div>`;
+                        } else if (node.data.uri.includes("youtube.com/embed")) {
+                            return `<div class="iframe-container">
+                                <iframe
+                                title="${next(node.content)}"
+                                src="${node.data.uri}"
+                                allow='accelerometer; encrypted-media; gyroscope; picture-in-picture'
+                                frameBorder='0'
+                                allowFullScreen
+                                ></iframe>
+                            </div>`;
+                        } else {
+                            return `<span class="color">
+                            <a href="${node.data.uri}">${next(node.content)}</a>
+                            </span>`;
+                        }
+                    },
+                    [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
+                        return `<div class="content-img"><img
+                            src="https:${node.data.target.fields.file.url}"
+                            width='100%'
+                            alt="${node.data.target.fields.description}"
+                            /></div>`;
+                    },
+                },
+            }
+
+            const { id, createdAt } = item.sys;
+            const { title, description } = item.fields;
+            const featuredImage = item.fields.featuredImage.fields.file.url
+            const content = item.fields.details;
+            const details = documentToHtmlString(content, renderOptions)
+            const customDate = getCustomDate(createdAt)
+
+            return{
+                id, title, featuredImage, description, details, createdAt, customDate
+            }
+        })
+        return {
+            blogDetails: blog
+        };
+    },
     data() {
         return {
             blogDetails: null,
@@ -127,84 +218,6 @@ export default {
             let newDate = `${dt} ${month}, ${year}`
             return newDate
         },
-        async getOneBlog(slug) {
-            var response = await this.$contentful.client.getEntries({
-                content_type: 'blog',
-                'fields.slug': slug
-            })
-            let blog = response.items;
-
-            blog = blog.map((item) => {
-                let renderOptions = {
-                    renderNode: {
-                        [BLOCKS.EMBEDDED_ENTRY]: (node, children) => {
-                        if (node.data.target.sys.contentType.sys.id === "codeBlock") {
-                            return `<pre>
-                                <code>${node.data.target.fields.code}</code>
-                            </pre>`;
-                        }
-
-                        if (node.data.target.sys.contentType.sys.id === "videoEmbed") {
-                            return `<iframe
-                                src='${node.data.target.fields.embedUrl}'
-                                height='100%'
-                                width='100%'
-                                frameBorder='0'
-                                scrolling='no'
-                                title='${node.data.target.fields.title}'
-                                allowFullScreen=true
-                            />`;
-                        }
-                        },
-                        [INLINES.HYPERLINK]: (node, next) => {
-                        if (node.data.uri.includes("player.vimeo.com/video")) {
-                            return `<div class="iframe-container">
-                                <iframe
-                                title="${next(node.content)}"
-                                src="${node.data.uri}"
-                                frameBorder='0'
-                                allowFullScreen
-                                ></iframe>
-                            </div>`;
-                        } else if (node.data.uri.includes("youtube.com/embed")) {
-                            return `<div class="iframe-container">
-                                <iframe
-                                title="${next(node.content)}"
-                                src="${node.data.uri}"
-                                allow='accelerometer; encrypted-media; gyroscope; picture-in-picture'
-                                frameBorder='0'
-                                allowFullScreen
-                                ></iframe>
-                            </div>`;
-                        } else {
-                            return `<span class="color">
-                            <a href="${node.data.uri}">${next(node.content)}</a>
-                            </span>`;
-                        }
-                        },
-                        [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
-                        return `<div class="content-img"><img
-                            src="https:${node.data.target.fields.file.url}"
-                            width='100%'
-                            alt="${node.data.target.fields.description}"
-                            /></div>`;
-                        },
-                    },
-                }
-
-                const { id, createdAt } = item.sys;
-                const { title, description } = item.fields;
-                const featuredImage = item.fields.featuredImage.fields.file.url
-                const content = item.fields.details;
-                const details = documentToHtmlString(content, renderOptions)
-                const customDate = this.getCustomDate(createdAt)
-
-                return{
-                    id, title, featuredImage, description, details, createdAt, customDate
-                }
-            })
-            this.blogDetails = blog
-        },
         async getPdf(slug) {
             var response = await this.$contentful.client.getEntries({
                 content_type: 'blog',
@@ -230,7 +243,6 @@ export default {
         }
     },
     mounted() {
-        this.getOneBlog(this.$route.params.id)
         this.getPdf(this.$route.params.id)
         this.getTwoBlogs()
     }
